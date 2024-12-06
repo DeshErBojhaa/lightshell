47d61c16da46011549f166393291bc087bfd2205
tamjidahmed <mail.to.tamjid@gmail.com>
2024-12-07 01:15:48 +0600

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
+
+fn token(input: &str) -> Vec<&str> {
+    input.split_whitespace().collect()
+}
 
 fn main() {
     loop {
@@ -7,9 +12,13 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        if input.trim().starts_with("exit") {
-            break;
+        let tok = token(input.trim());
+        match tok[..] { 
+            ["exit", code] => {
+                process::exit(code.parse::<i32>().unwrap());
+            },
+            ["echo", ..] => println!("{}", tok[1..].join(" ")),
+            _ => println!("{}: not found", input.trim())  
         }
-        println!("{}: not found", input.trim());
     }
 }
